6ec6fff1a6525fb004661043686bc26329e77bde
N.Dorves <dorves@gmx.net>
2024-08-22 01:33:02 +0200

    add HAFG_35

---
+++ Descr_db.py
@@ -0,0 +1,24 @@
+# import mysql.connector
+#
+# dbconfig = {
+#     'host': 'host',
+#     'user': 'user',
+#     'password': 'password',
+#     'database': 'sakila'
+# }
+#
+#
+# def get_connection():
+#     return mysql.connector.connect(**dbconfig)
+#
+#
+# dbconfig2 = {
+#     'host': 'host',
+#     'user': 'user',
+#     'password': 'password',
+#     'database': '310524ptm_Dorves'
+# }
+#
+#
+# def get_connection2():
+#     return mysql.connector.connect(**dbconfig2)
